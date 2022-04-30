69c35c183c6df657454fd2e7a09adf2b64a94ef6
mobiledev15 <revze@outlook.co.id>
2022-04-30 20:44:23 +0700

    feat(genetic_drawing): add calcBrushRange, set_brush_range, set_sampling_mask, create_sampling_mask and _calcBrushSize method

--- genetic_drawing.py
+++ genetic_drawing.py
@@ -12,3 +12,27 @@ class GeneticDrawing:
         
         #start with an empty black img
         self.imgBuffer = [np.zeros((self.img_grey.shape[0], self.img_grey.shape[1]), np.uint8)]
+
+    def calcBrushRange(self, stage, total_stages):
+        return [self._calcBrushSize(self.brushesRange[0], stage, total_stages), self._calcBrushSize(self.brushesRange[1], stage, total_stages)]
+        
+    def set_brush_range(self, ranges):
+        self.brushesRange = ranges
+        
+    def set_sampling_mask(self, img_path):
+        self.sampling_mask = cv2.cvtColor(cv2.imread(img_path),cv2.COLOR_BGR2GRAY)
+        
+    def create_sampling_mask(self, s, stages):
+        percent = 0.2
+        start_stage = int(stages*percent)
+        sampling_mask = None
+        if s >= start_stage:
+            t = (1.0 - (s-start_stage)/max(stages-start_stage-1,1)) * 0.25 + 0.005
+            sampling_mask = self.calc_sampling_mask(t)
+        return sampling_mask
+
+    def _calcBrushSize(self, brange, stage, total_stages):
+        bmin = brange[0]
+        bmax = brange[1]
+        t = stage/max(total_stages-1, 1)
+        return (bmax-bmin)*(-t*t+1)+bmin
